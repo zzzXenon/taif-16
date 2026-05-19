319251adfce9bc3227deed87d4492d21c838ad78
zzzXenon <royjonathan03082003@gmail.com>
2026-05-19 08:24:04 +0700

    feat: Add proxy support for Jupyter Server Proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: 'all',
+    cors: true
+  }
 })
